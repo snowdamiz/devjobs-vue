31bbd4adce76d846d76dc4982515b2d7deb7077f
Andrey Yurlov <yurlovandrew@gmail.com>
2022-03-16 15:04:52 -0500

    added styled components and test heading

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
+    <StyledH1>test</StyledH1>
   </div>
 </template>
 
 <script>
+  import { StyledH1 } from './styles/micro/micro.styles'
+
   export default {
     name: 'App',
+    components: {
+      StyledH1
+    }
   }
 </script>
 
 <style>
+  @import url('https://fonts.googleapis.com/css2?family=Kumbh+Sans&display=swap');
+
 </style>
---
+++ src/styles/micro/micro.styles.js
@@ -0,0 +1,6 @@
+import styled from 'vue-styled-components'
+import { fonts } from '../../style.config'
+
+export const StyledH1 = styled.h1`
+    font-family: ${fonts.primary};
+`
