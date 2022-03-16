ef34f2f3caae6724537342a65332db3d327067fe
Andrey Yurlov <yurlovandrew@gmail.com>
2022-03-16 14:53:53 -0500

    add style config

---
+++ src/style.config.js
@@ -0,0 +1,14 @@
+export const colors = {
+    violet: "#5964E0",
+    lightViolet: "#939BF4",
+    veryDarkBlue: "#19292D",
+    midnight: "#121721",
+    white: "#FFFFFF",
+    lightGray: "#F4F6F8",
+    gray: "#9DAEC2",
+    darkGray: "#6E8090",
+}
+
+export const fonts = {
+    primary: `'Kumbh Sans', sans-serif`
+}
