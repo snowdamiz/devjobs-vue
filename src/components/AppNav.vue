cfcc8a855f78e4b89c7dc22c7e16c390321da7e4
Andrey Yurlov <yurlovandrew@gmail.com>
2022-03-21 14:00:53 -0500

    fix clean up

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -30,27 +30,6 @@
 <script>
   export default {
     name: 'AppNav',
-    data() {
-      return {
-        window: {
-          width: 0,
-          height: 0,
-        },
-      }
-    },
-    created() {
-      window.addEventListener('resize', this.handleResize)
-      this.handleResize();
-    },
-    destroyed() {
-      window.removeEventListener('resize', this.handleResize)
-    },
-    methods: {
-      handleResize() {
-        this.window.width = window.innerWidth;
-        this.window.height = window.innerHeight;
-      },
-    },
   }
 </script>
 
